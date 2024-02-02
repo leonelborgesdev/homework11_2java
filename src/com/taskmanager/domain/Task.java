6d6311a0ea0bc92328bdee570a5aa570249e41fd
leonelborgesdev <leonel03t@gmail.com>
2024-02-01 23:12:17 -0400

    task domain class

--- src/com/taskmanager/domain/Task.java
+++ src/com/taskmanager/domain/Task.java
@@ -1,4 +1,55 @@
 package com.taskmanager.domain;
 
+import java.time.LocalDate;
+
 public class Task {
+    private long id;
+    private String title;
+    private String description;
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    private LocalDate dueDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public LocalDate getDueDate() {
+//        return dueDate;
+//    }
+//
+//    public void setDueDate(LocalDate dueDate) {
+//        this.dueDate = dueDate;
+//    }
+
+    // toString method for displaying task information
+    @Override
+    public String toString() {
+        return "com.taskmanager.domain.Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+//                ", dueDate=" + dueDate +
+                '}';
+    }
 }
+
