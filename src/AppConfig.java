6a3d8c852b71f5e1599037750f766ce3e6953f41
leonelborgesdev <leonel03t@gmail.com>
2024-02-01 23:07:01 -0400

    task2

---
+++ src/AppConfig.java
@@ -0,0 +1,10 @@
+import com.taskmanager.domain.Task;
+import com.taskmanager.domain.TaskService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class AppConfig {
+    public static void main(String[] args) {
+    }
+}
